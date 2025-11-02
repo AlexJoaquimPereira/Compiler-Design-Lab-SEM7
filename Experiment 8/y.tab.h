6685a91240ed9646252fa493b20836f013fe85e1
AlexJoaquimPereira <alexpereira1781@gmail.com>
2025-11-03 00:41:13 +0530

    C header file containing the integer definitions

---
+++ Experiment 8/y.tab.h
@@ -0,0 +1,14 @@
+#ifndef _yy_defines_h_
+#define _yy_defines_h_
+
+#define FOR 257
+#define ID 258
+#define NUMBER 259
+#define T_EQ 260
+#define T_NEQ 261
+#define T_LTE 262
+#define T_GTE 263
+#define T_INC 264
+#define T_DEC 265
+
+#endif /* _yy_defines_h_ */
